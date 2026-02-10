a07d2ef44a31da274880145bb0846213ea48edf6
Zac Libman <zaclibman@Mac.lan>
2026-02-09 16:36:41 -0800

    Update

---
+++ public/assets/app.init.202602100024-c7d3d95.js
@@ -0,0 +1,3 @@
+// LiveNew init shim. verify-assets requires this file to import ONLY bootstrapApp.
+import { bootstrapApp } from "./app.core.202602100024-c7d3d95.js";
+bootstrapApp();
---
+++ public/assets/build.202602100024-c7d3d95.js
@@ -0,0 +1,3 @@
+export const BUILD_ID = "202602100024-c7d3d95";
+export const APP_CORE_SPEC = "./app.core.js?v=" + BUILD_ID;
+export const APP_STATE_SPEC = "./app.state.js?v=" + BUILD_ID;
