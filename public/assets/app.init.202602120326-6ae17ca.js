d36bb47dcbecc5873736da152dee87f40f664936
Zac Libman <zaclibman@Mac.lan>
2026-02-11 20:01:23 -0800

    Update

---
+++ public/assets/app.init.202602120326-6ae17ca.js
@@ -0,0 +1,2 @@
+import { bootstrapApp } from "./app.core.202602120326-6ae17ca.js";
+bootstrapApp();
---
+++ public/assets/build.202602120326-6ae17ca.js
@@ -0,0 +1,3 @@
+export const BUILD_ID = "202602120326-6ae17ca";
+export const APP_CORE_SPEC = "./app.core.js?v=" + BUILD_ID;
+export const APP_STATE_SPEC = "./app.state.js?v=" + BUILD_ID;
