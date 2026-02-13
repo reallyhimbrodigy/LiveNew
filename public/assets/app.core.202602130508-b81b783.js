f873515995be2c16a4f64340e70a0eff6792adb9
Zac Libman <zaclibman@Mac.lan>
2026-02-13 09:45:50 -0800

    Update

--- public/assets/app.core.202602130508-b81b783.js
+++ public/assets/app.core.202602130508-b81b783.js
@@ -13,9 +13,9 @@ import {
   logoutAuth,
   setDeviceName,
   getDeviceName,
-} from "./app.api.js";
-import { getAppState as getAppStateInternal, setAppState } from "./app.state.js";
-import { qs, qsa, el, clear, setText, formatMinutes, formatPct, applyI18n, getDictValue } from "./app.ui.js";
+} from "./app.api.202602130508-b81b783.js";
+import { getAppState as getAppStateInternal, setAppState } from "./app.state.202602130508-b81b783.js";
+import { qs, qsa, el, clear, setText, formatMinutes, formatPct, applyI18n, getDictValue } from "./app.ui.202602130508-b81b783.js";
 import { STRINGS as EN_STRINGS } from "../i18n/en.js";
 /* REQUIRED: build-time export used by controllers + asset verification */
 export function getAppState() {
@@ -25,7 +25,7 @@ export function getAppState() {
   return {};
 }
 void getAppStateInternal;
-export const BUILD_ID = "__BUILD_ID__";
+export const BUILD_ID = "202602130508-b81b783";
 
 const LOCALE = "en";
 const STRINGS = { en: EN_STRINGS }[LOCALE] || EN_STRINGS;
@@ -511,7 +511,7 @@ async function updateAdminVisibility() {
 let controllersModule = null;
 async function loadControllers() {
   if (!controllersModule) {
-    controllersModule = import("./controllers.js");
+    controllersModule = import("./controllers.202602130508-b81b783.js");
   }
   return controllersModule;
 }
---
+++ public/assets/app.init.202602130508-b81b783.js
@@ -0,0 +1,2 @@
+import { bootstrapApp } from "./app.core.202602130508-b81b783.js";
+bootstrapApp();
