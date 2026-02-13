95315ade7bac44acb8bda6a22bf0dd842c4d70a0
Zac Libman <zaclibman@Mac.lan>
2026-02-13 14:47:51 -0800

    fix: rebuild assets to replace stale 202602132214 bundle with correct auth flow

---
+++ public/assets/app.init.202602132238-e7f0999.js
@@ -0,0 +1,2 @@
+import { bootstrapApp } from "./app.core.202602132238-e7f0999.js";
+bootstrapApp();
---
+++ public/assets/app.init.202602132247-e7f0999.js
@@ -0,0 +1,2 @@
+import { bootstrapApp } from "./app.core.202602132247-e7f0999.js";
+bootstrapApp();
